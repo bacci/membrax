5d2f02f1e561c7e04e41b73bad82233f2481b849
Giancarlo Bacci <giancarlo.baci@gmail.com>
2019-08-02 18:05:44 -0300

    Adicionado novo index e rota

--- src/routes.php
+++ src/routes.php
@@ -7,7 +7,7 @@ use Slim\Http\Response;
 return function (App $app) {
     $container = $app->getContainer();
 
-	$app->get('/home', function (Request $request, Response $response, array $args) use ($container) {
+	$app->get('/', function (Request $request, Response $response, array $args) use ($container) {
 
 		$nome_app = $container->get('settings')['application']['name'];
 
